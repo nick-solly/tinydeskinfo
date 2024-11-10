958c62b3a93433eabe321761c492f465f022d220
Nick Solly <nick@mohara.co>
2024-11-10 16:11:43 +0000

    chore: add db fetch logging

--- src/lib/videos.ts
+++ src/lib/videos.ts
@@ -4,17 +4,17 @@ import { type VideoRow } from "~/lib/schemas";
 import { desc, ilike } from "drizzle-orm";
 import { videos } from "~/server/db/schema";
 
-export const getVideos = cache(
-  async (): Promise<VideoRow[]> =>
-    db.query.videos.findMany({
-      columns: {
-        id: true,
-        title: true,
-        viewCount: true,
-        duration: true,
-        publishedAt: true,
-      },
-      where: ilike(videos.title, "%tiny desk%"),
-      orderBy: [desc(videos.publishedAt)],
-    }),
-);
+export const getVideos = cache(async (): Promise<VideoRow[]> => {
+  console.log("Fetching videos from DB");
+  return db.query.videos.findMany({
+    columns: {
+      id: true,
+      title: true,
+      viewCount: true,
+      duration: true,
+      publishedAt: true,
+    },
+    where: ilike(videos.title, "%tiny desk%"),
+    orderBy: [desc(videos.publishedAt)],
+  });
+});
